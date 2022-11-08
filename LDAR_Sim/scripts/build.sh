5e714e7f7e8fb84a0ba508493503ca01b0ac5cf4
MozhouGaoS <115584176+MozhouGaoS@users.noreply.github.com>
2022-11-08 13:07:44 -0700

    Update build.sh

--- LDAR_Sim/scripts/build.sh
+++ LDAR_Sim/scripts/build.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 rm -rf build
 mkdir -p build/{inputs,sim_test,src} 
-cd .. 
-cp -r ./src/* scripts/build/src 
-cp -r ./inputs/* scripts/build/inputs
-cp -r ./sim_test/* scripts/build/sim_test
-cd scripts 
-pipenv requirements > requirements.txt
+cp ./src/* build/src
+cp ./src/* build/inputs
+cp ./src/* build/sim_test
+pipenv lock -r > requirements.txt
 pip install -r requirements.txt --no-deps -t build
 rm -f ldar-sim.zip
-cd build
-zip -r ../ldar-sim.zip . -i *
+cd build; zip -r ../ldar-sim.zip *
