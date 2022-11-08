15417d6c151008dd201e7f78e8cd5bc421593525
Mozhou Gao <mozhou@ualberta.ca>
2022-11-08 09:47:03 -0700

    update build file

--- LDAR_Sim/scripts/build.sh
+++ LDAR_Sim/scripts/build.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 rm -rf build
-mkdir -p build/{inputs,sim_test,src}
-# mkdir build/inputs 
-# mkdir build/sim_test 
-# mkdir build/src 
+mkdir -p build/{inputs,sim_test,src} 
 cd .. 
 cp -r ./src/* scripts/build/src 
 cp -r ./inputs/* scripts/build/inputs
@@ -13,5 +10,4 @@ pipenv requirements > requirements.txt
 pip install -r requirements.txt --no-deps -t build
 rm -f ldar-sim.zip
 cd build
-mkdir fakezip 
-#zip -r ../ldar-sim.zip *
+zip -r ../ldar-sim.zip *
