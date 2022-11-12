f29b0bf3f127f044526dc00097bd8314258c0053
MozhouGaoS <115584176+MozhouGaoS@users.noreply.github.com>
2022-11-11 18:03:21 -0700

    Update build.sh

--- LDAR_Sim/scripts/build.sh
+++ LDAR_Sim/scripts/build.sh
@@ -6,18 +6,10 @@ mkdir -p src
 mkdir -p inputs 
 mkdir -p sim_test
 cd .. 
-<<<<<<< HEAD
 cp -r ./src/. build/src 
-=======
-cp ./src/*.py build/src
->>>>>>> 7c5e5a4d2d94998a0db16c0c4af2ac0cf526da59
 cp ./inputs/* build/inputs
 cp ./sim_test/* build/sim_test
 pipenv lock -r > requirements.txt
 pip install -r requirements.txt --no-deps -t build
 rm -f ldar-sim.zip
-<<<<<<< HEAD
 cd build; zip -r ../ldar-sim.zip *
-=======
-cd build; zip -r ../ldar-sim.zip *
->>>>>>> 7c5e5a4d2d94998a0db16c0c4af2ac0cf526da59
